c676907e895ec1b4195289a3e22c9e6d5af56c8b
ampereprotocol <132098191+ampereprotocol@users.noreply.github.com>
2025-04-12 12:30:19 +0100

    Update mail.php
    Signed-off-by: ampereprotocol <132098191+ampereprotocol@users.noreply.github.com>

--- assets/mail/mail.php
+++ assets/mail/mail.php
@@ -15,7 +15,7 @@ Message: ".$message."
 
 ";
 
-mail ("example@gmail.com" , "New Message", $email_message);
+mail ("ampereprotocol@gmail.com" , "New Message", $email_message);
 header("location: ../../mail-success.html");
 ?>
 
